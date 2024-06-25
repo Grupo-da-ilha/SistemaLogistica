f8014c6705be82eac841f5c23f43c204f0adf589
Elias Alves Lima <137322132+elijahAlvs@users.noreply.github.com>
2024-06-25 07:27:28 -0300

    atualização
    .

--- Paginas/carga.php
+++ Paginas/carga.php
@@ -8,8 +8,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="description" content="SENAI Supply Chain Solutions">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/menuhorizontal.css"/>
     <link rel="stylesheet" href="../css/carga.css"/>
+    <link rel="stylesheet" href="../css/menuhorizontal.css"/>
     <link rel="shortcut icon" type="imagex/png" href="#"/>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
