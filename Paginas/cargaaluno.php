f2a6356627e6116d1ed46f1ba7f9dd642de8a578
Jonas_F_M <126809250+DZJonasYI@users.noreply.github.com>
2024-08-01 08:14:56 -0300

    redirecionamento

--- Paginas/cargaaluno.php
+++ Paginas/cargaaluno.php
@@ -84,8 +84,7 @@ if (empty($_SESSION['nome'])){
                     <a href="estoquealuno.php" class="functions-menu">ESTOQUE</a>
                     <a href="movimentacaoaluno.php" class="functions-menu">MOVIMENTAÇÃO</a>
                     <a href="pickingaluno.php" class="functions-menu">PICKING</a>
-                    <a href="expediçaoaluno.php" class="functions-menu">EXPEDIÇÃO</a>
-                    <a href="relatoriosaluno.php" class="functions-menu">RELATÓRIOS</a>
+                    <a href="expedicaoaluno.php" class="functions-menu">EXPEDIÇÃO</a>
                 </li>
             </div>
             <div class="recebimentocontainer">
