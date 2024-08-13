fb429e994aef96aa78038508615c1d81915cd06d
silvioFR <165297716+silvioFR@users.noreply.github.com>
2024-08-13 08:12:14 -0300

    .
    .

--- Paginas/estoque.php
+++ Paginas/estoque.php
@@ -96,7 +96,6 @@ if (empty($_SESSION['nome'])){
                     <a href="carga.php" class="functions-menu">VISTORIA</a>
                     <a href="recebimentodoca.php" class="functions-menu">RECEBIMENTO</a>
                     <a href="controledoca.php" class="functions-menu">CONTROLE</a>
-                    <a href="estoque.php" class="functions-menu">ESTOQUE</a>
                     <a href="movimentacao.php" class="functions-menu">MOVIMENTAÇÃO</a>
                     <a href="picking.php" class="functions-menu">PICKING</a>
                     <a href="expedicao.php" class="functions-menu">EXPEDIÇÃO</a>
