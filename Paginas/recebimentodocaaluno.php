fc0c9ce975afc49df3c77efc6c206cca2cba153f
Kauan Morais Burger <kauanburger007@gmail.com>
2024-08-18 12:32:49 -0300

    Update recebimentodocaaluno.php

--- Paginas/recebimentodocaaluno.php
+++ Paginas/recebimentodocaaluno.php
@@ -112,7 +112,7 @@ if (empty($_SESSION['nome'])){
                                 <div class="produtos-pedido">
                                     <h4>PEDIDOS:</h4>';
 
-
+    $temQuantidadeNaoZero = false;
     $sql = "SELECT * FROM pedido WHERE codTurma ='$codTurma' AND Situacao = 'Nas docas'";
     $executar = $conexao->query($sql);
 
@@ -138,33 +138,33 @@ if (empty($_SESSION['nome'])){
 
             if($executeItem && $executeItem->num_rows > 0){
                 while($row = $executeItem->fetch_assoc()){
-                    $faltando = $conexao->real_escape_string($row['Faltando']);
-                    $avariado = $conexao->real_escape_string($row['Avariado']);
-                    $QttDoca = $conexao->real_escape_string($row['Quantidade_doca']);
+                    $faltando = $row['Faltando'];
+                    $avariado = $row['Avariado'];
+                    $QttDoca = $row['Quantidade_doca'];
 
-                    if($QttDoca == 0){
-                        $sqlUpdatepedido = "UPDATE pedido SET Situacao = 'Em movimentação' WHERE codTurma ='$codTurma' AND id_pedido = '$idpedidos'";
-                        $executeUpdate = $conexao->query($sqlUpdatepedido);
+                    if ($QttDoca != 0) {
+                        $temQuantidadeNaoZero = true;
                     }
 
-                    if($avariado == 1 && $faltando == 0){
-                        $Avariado = 'SIM';
-                        $Faltando = 'Não';
-                    } elseif($faltando == 1 && $avariado == 0){
-                        $Faltando = 'SIM';
-                        $Avariado = 'Não';
-                    } elseif($avariado == 1 && $faltando == 1){
-                        $Avariado = 'SIM';
-                        $Faltando = 'SIM';
-                    } else{
-                        $Avariado = 'Não';
-                        $Faltando = 'Não';
+                    if ($avariado == 1) {
+                        $temAvariado = true;
                     }
-                
+                    if ($faltando == 1) {
+                        $temFaltando = true;
+                    }
+                }
+
+                if (!$temQuantidadeNaoZero) {
+                    $sqlUpdatepedido = "UPDATE pedido SET Situacao = 'Em movimentação' WHERE codTurma = '$codTurma' AND id_pedido = '$idpedidos'";
+                    $executeUpdate = $conexao->query($sqlUpdatepedido);
+                }
+
+                $Avariado = $temAvariado ? 'SIM' : 'Não';
+                $Faltando = $temFaltando ? 'SIM' : 'Não';
 
             $sqlDoca = "SELECT * FROM docas WHERE codTurma ='$codTurma' AND id_pedido = '$idpedidos'";
             $executeDoca = $conexao->query($sqlDoca);
-                }
+            
             if($executeDoca && $executeDoca->num_rows > 0){
                 while($rowDoca = $executeDoca->fetch_assoc()){
                     echo '<tr>
